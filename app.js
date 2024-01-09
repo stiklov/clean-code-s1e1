4b0afed0b6263c74a33d806fd1d3df95371defd0
Димка Стиклов <dimkastiklov@Dimkas-MacBook-Pro.local>
2024-01-09 06:16:41 +0400

    fix(extended-1.2): add alt for medias

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ var createNewTaskElement=function(taskString){
   var deleteButton=document.createElement("button");//delete button
   var deleteButtonImg=document.createElement("img");//delete button image
 
+  deleteButtonImg.alt="Remove image";
+
   label.innerText=taskString;
   label.className="incompleted-label";
 
